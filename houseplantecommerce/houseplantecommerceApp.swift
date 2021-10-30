cde473df0df6cc5d7185e106e21f0c6a5ff233a9
yakintech <cagatayyildiz87@gmail.com>
2021-10-30 12:53:49 +0300

    Initial Commit

---
+++ houseplantecommerce/houseplantecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  houseplantecommerceApp.swift
+//  houseplantecommerce
+//
+//  Created by Çağatay Yıldız on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct houseplantecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
