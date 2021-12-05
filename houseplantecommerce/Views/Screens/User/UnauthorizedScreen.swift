8f7ac394d719a6e5045dff05764a7636b85a3907
burak-cakir <burakcakir@hotmail.com.tr>
2021-12-06 01:22:12 +0300

    UnauthorizedScreen new button design

--- houseplantecommerce/Views/Screens/User/UnauthorizedScreen.swift
+++ houseplantecommerce/Views/Screens/User/UnauthorizedScreen.swift
@@ -14,9 +14,22 @@ struct UnauthorizedScreen: View {
                 NavigationLink(destination: RegisterScreen(), label:{
                     Text("Register")
                 })
+                    .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    .frame(width: 200, height: 60)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+                    .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+               
                 NavigationLink(destination: LoginScreen(), label:{
                     Text("Login")
                 })
+                    .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    .frame(width: 200, height: 60)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+                    .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
             }
             
         }
